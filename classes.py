3e89a2405408795024c33de378083338539edc66
ForestRico <126733661+ForestRico@users.noreply.github.com>
2023-07-10 06:20:43 +1000

    implemented classes

--- classes.py
+++ classes.py
@@ -1,31 +1,74 @@
-class Booking: 
-    def __init__(self, client_name, contact_no, service, date):
-        self.day = client_name
-        self.time = contact_no
-        self.service = service
-        self.date = date 
+class booking(): 
+    def __init__(self, customer_name, contact_details, datetime, service_type):
+        self.customer_name = customer_name
+        self.contact_details = contact_details
+        self.datetime = datetime
+        self.service_type = service_type
 
-class COD:
-    def __init__(self, title, map, gun, skin, lethal):
-        self.title = title 
-        self.map = map
-        self.gun = gun
-        self.skin = skin
-        self.lethal = lethal 
+        
 
-    def enter_data(self):
-        self.title = input("Please enter favourite COD Game: ")
-        self.map = input("COD Map: ")
-        self.gun = input("COD Gun: ")
-        self.skin = input("COD Skin: ")
-        self.lethal = input("COD Lethal: ")
 
-    def print_data(self):
-        print("Your favourite COD Game is: " + self.title)
-        print("Your favourite COD Map is : " + self.map)
-        print("Your favourite COD Gun is : " + self.gun)
-        print("Your favourite COD Skin is : " + self.skin)
-        print("Your favourite COD Lethal is: " + self.lethal)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# class Gun: 
+#     def __init__(self, title, type, select_fire, mag_size):
+#          self.title = title 
+#          self.type = type 
+#          self.select_fire = select_fire 
+#          self.mag_size = mag_size
+    
+#     def is_a_sniper(self):
+#         if self.select_fire == "Bolt Action":
+#             return True
+#         else: 
+#             return False
+        
+    
+# class Booking: 
+#     def __init__(self, client_name, contact_no, service, date):
+#         self.day = client_name
+#         self.time = contact_no
+#         self.service = service
+#         self.date = date 
+
+# class COD:
+#     def __init__(self, title, map, gun, skin, lethal):
+#         self.title = title 
+#         self.map = map
+#         self.gun = gun
+#         self.skin = skin
+#         self.lethal = lethal 
+
+#     def enter_data(self):
+#         self.title = input("Please enter favourite COD Game: ")
+#         self.map = input("COD Map: ")
+#         self.gun = input("COD Gun: ")
+#         self.skin = input("COD Skin: ")
+#         self.lethal = input("COD Lethal: ")
+
+#     def print_data(self):
+#         print("Your favourite COD Game is: " + self.title)
+#         print("Your favourite COD Map is : " + self.map)
+#         print("Your favourite COD Gun is : " + self.gun)
+#         print("Your favourite COD Skin is : " + self.skin)
+#         print("Your favourite COD Lethal is: " + self.lethal)
 
     
         
--- main.py
+++ main.py
@@ -1,35 +1,86 @@
-from classes import COD
-import datetime 
+from classes import booking 
 
-class COD(object):
-    def __init__(self, title, map, gun, skin, lethal):
-        self.title = title 
-        self.map = map
-        self.gun = gun
-        self.skin = skin
-        self.lethal = lethal 
-
-    def enter_data(self):
-        self.title = input("Please enter favourite COD Game: ")
-        self.map = input("COD Map: ")
-        self.gun = input("COD Gun: ")
-        self.skin = input("COD Skin: ")
-        self.lethal = input("COD Lethal: ")
-
-    def print_data(self):
-        print("Your favourite COD Game is: " + self.title)
-        print("Your favourite COD Map is : " + self.map)
-        print("Your favourite COD Gun is : " + self.gun)
-        print("Your favourite COD Skin is : " + self.skin)
-        print("Your favourite COD Lethal is: " + self.lethal)
+def menu():
+    choice = input("Welcome to MK Motorsports!" + "\n" "Would you like to book a service?" + "\n" "Type Y or N: ")
+    if choice == "Y" or choice == "y":
+        print("Thanks for choosing us")
+        return booking_service == True
+    elif choice == "N" or choice == "n":
+        print("Thank you for visiting us!")
+    else: 
+        print("Invalid Input")
+        
+# def booking_system():
+#     if booking_service == True:
+#         input("")
 
 
-a = COD()
-L = []
 
 
-a.enter_data() 
-L.append(a)
+
+
+booking_service = True
+menu()
+booking_input()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# from classes import Gun
+
+# Galil = Gun("Galil", "Rifle", "Automatic", 30)
+# AK47 = Gun("AK47", "Rifle", "Automatic", 30)
+# AWP = Gun("AWP", "Sniper", "Bolt Action", 6)
+# print(AWP.select_fire)
+# print(AWP.is_a_sniper())
+
+# from classes import COD
+
+
+
+
+# from classes import COD
+# import datetime 
+
+# class COD(object):
+#     def __init__(self, title, map, gun, skin, lethal):
+#         self.title = title 
+#         self.map = map
+#         self.gun = gun
+#         self.skin = skin
+#         self.lethal = lethal 
+
+#     def enter_data(self):
+#         self.title = input("Please enter favourite COD Game: ")
+#         self.map = input("COD Map: ")
+#         self.gun = input("COD Gun: ")
+#         self.skin = input("COD Skin: ")
+#         self.lethal = input("COD Lethal: ")
+
+#     def print_data(self):
+#         print("Your favourite COD Game is: " + self.title)
+#         print("Your favourite COD Map is : " + self.map)
+#         print("Your favourite COD Gun is : " + self.gun)
+#         print("Your favourite COD Skin is : " + self.skin)
+#         print("Your favourite COD Lethal is: " + self.lethal)
+
+
+# a = COD()
+# L = []
+
+
+# a.enter_data() 
+# L.append(a)
 
 
 

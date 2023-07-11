e22ff9754c5a1853db3c6d3e80f735d667ea083f
ForestRico <126733661+ForestRico@users.noreply.github.com>
2023-07-11 18:54:34 +1000

    Source is in Main2, fixed booking_menu()

--- main2.py
+++ main2.py
@@ -11,49 +11,34 @@ class Booking():
     
 booking_storage = {}
 
-
-
-
 def booking_menu():
     while True:
         print("Welcome to MK Motorsports")
         print("1. Create Booking")
         print("2. Show All Bookings")
-        print("3. Exit")
+        print("3. Edit a Booking")
+        print("4. Exit")
         choice = input("Enter Choice (1-3): ")
         if choice == "1":
-            booking_data = {}
             customer_name = input("Please enter your fullname: ")
             customer_phone = input("Please enter your phone number: ")
             date = input("Please enter the date DD-MM-YY: ")
             time = input("Please enter the time HH-MM: ")
             service_type = input("Please enter service type: ")
 
-            booking_data["name"] = customer_name
-            booking_data["phone"] = customer_phone
-            booking_data["date_e"] = date
-            booking_data["time_e"] = time
-            booking_data["service"] = service_type
-
-            booking_storage[customer_name] = booking_data
-            print(booking_storage[customer_name])
-
-            # booking_storage[customer_name] = booking_data
-            # print(booking_data[customer_name])
-
-
-
-            # c1 = Booking(customer_name, customer_phone, date, time, service_type)
-            # booking_data.append(c1)
+            booking = Booking(customer_name, customer_phone, date, time, service_type)
+            booking_storage[customer_name] = booking
+ 
+        
 
         elif choice == "2":
-            for index, bookings in enumerate(booking_data):
+            for index, bookings in enumerate(booking_storage.values()):
                 print("-----------------")
                 print(f"Booking {index+1}: {bookings}")
                 print("-----------------")
-            # booking_data.show_bookings()
+           
         
-        elif choice == "3":
+        elif choice == "4":
             print("Thanks For Using Our Service!")
             return False
             

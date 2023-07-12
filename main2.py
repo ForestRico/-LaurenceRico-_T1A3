b607b253433373103abbd1b558edcbf5b03856d6
ForestRico <126733661+ForestRico@users.noreply.github.com>
2023-07-12 15:10:18 +1000

    added Find your Booking to menu

--- main2.py
+++ main2.py
@@ -13,11 +13,12 @@ booking_storage = {}
 
 def booking_menu():
     while True:
-        print("Welcome to MK Motorsports")
+        print("Welcome to elRico Motorsports")
         print("1. Create Booking")
-        print("2. Show All Bookings")
-        print("3. Edit a Booking")
-        print("4. Exit")
+        print("2. Find your Booking")
+        print("3. Show All Bookings")
+        print("4. Edit a Booking")
+        print("5. Exit")
         choice = input("Enter Choice (1-3): ")
         if choice == "1":
             customer_name = input("Please enter your fullname: ")
@@ -29,16 +30,31 @@ def booking_menu():
             booking = Booking(customer_name, customer_phone, date, time, service_type)
             booking_storage[customer_name] = booking
  
-        
-
         elif choice == "2":
+            name_input = input("Please enter name associated with Booking: ") 
+            found_booking = False
+            while not found_booking:
+                for name_find in booking_storage:
+                    if name_find == name_input:    
+                        print(booking_storage[name_input])
+                        found_booking = True
+                        break 
+                if not found_booking:
+                    print("Name and booking not found")
+                    name_input = input("Please enter a valid name associated with a booking")
+            # add .lower() function to name_input so that the user doesnt have to worry about being case sensitive
+
+
+        elif choice == "3":
             for index, bookings in enumerate(booking_storage.values()):
                 print("-----------------")
                 print(f"Booking {index+1}: {bookings}")
                 print("-----------------")
-           
-        
-        elif choice == "4":
+
+        # elif choice == "4":
+            
+    
+        elif choice == "5":
             print("Thanks For Using Our Service!")
             return False
             

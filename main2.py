e4d16f1073998cff39676b098930c4b33082a914
ForestRico <126733661+ForestRico@users.noreply.github.com>
2023-07-12 18:46:16 +1000

    service storage

--- main2.py
+++ main2.py
@@ -1,15 +1,33 @@
 class Booking():
-    def __init__(self, customer_name, customer_phone, date, time, service_type):
+    def __init__(self, customer_name, customer_phone, date, time, service_types, cost):
         self.customer_name = customer_name 
         self.customer_phone = customer_phone
         self.date = date
         self.time = time
-        self.service_type = service_type
+        self.service_types = service_types
+        self.cost = cost
 
     def __str__(self):
         return f"Booking : {self.customer_name}, {self.customer_phone}, {self.date}, {self.time}, {self.service_type}"
     
 booking_storage = {}
+
+service_package = {
+    "1" : 300, 
+    "2" : 500,
+    "3" : 1000,
+    "4" : 750,
+    "5" : 1000
+}
+
+service_type_dict = {
+    "1" : "Photography",
+    "2" : "Videography",
+    "3" : "Website Services",
+    "4" : "Marketing Services",
+    "5" : "Advertisements",
+}
+
 
 def booking_menu():
     while True:
@@ -19,16 +37,47 @@ def booking_menu():
         print("3. Show All Bookings")
         print("4. Edit a Booking")
         print("5. Exit")
-        choice = input("Enter Choice (1-3): ")
+        choice = input("Enter Choice (1-5): ")
         if choice == "1":
             customer_name = input("Please enter your fullname: ")
             customer_phone = input("Please enter your phone number: ")
             date = input("Please enter the date DD-MM-YY: ")
             time = input("Please enter the time HH-MM: ")
-            service_type = input("Please enter service type: ")
 
-            booking = Booking(customer_name, customer_phone, date, time, service_type)
-            booking_storage[customer_name] = booking
+            print("Please enter service type: \n1. Photography\n2. Videography\n3. Website Services\n4. Marketing Services\5. Advertisements")
+            service_choice = input("Enter Choice(s) serparated by commas (1-5): ")
+            service_choices = service_choice.split(",")
+
+            price = 0 
+            chosen_services = []
+
+            for key in service_package:
+                if key in service_choices:
+                    price += service_package[key]
+                    chosen_services.append(service_type_dict[key])
+                    # if key and service_choice(user input) match, then the users choices will append on a list 
+            
+            print("You have chosen the following services: ")
+            for services in chosen_services:
+                print(services)
+
+            print("Total Cost: " + str(price))
+            service_types = ", ".join(chosen_services)
+            # Combining the service types into a comma-separated string
+
+            # if key in service_type_dict:
+            #     print(key)
+
+            # for service_no in service_choice:
+            #     if service_no in service_type_dict:
+            #         print("You have chosen the following services: ")
+            #         for service_no in service_choice:
+            #             service_types = (service_type_dict[service_no])
+            #             print(service_types + "\nTotal Cost: " + str(price))
+            #             return service_types
+
+            # booking = Booking(customer_name, customer_phone, date, time, service_types, cost)
+            # booking_storage[customer_name] = booking
  
         elif choice == "2":
             name_input = input("Please enter name associated with Booking: ") 
@@ -44,7 +93,6 @@ def booking_menu():
                     name_input = input("Please enter a valid name associated with a booking")
             # add .lower() function to name_input so that the user doesnt have to worry about being case sensitive
 
-
         elif choice == "3":
             for index, bookings in enumerate(booking_storage.values()):
                 print("-----------------")
@@ -52,7 +100,6 @@ def booking_menu():
                 print("-----------------")
 
         # elif choice == "4":
-            
     
         elif choice == "5":
             print("Thanks For Using Our Service!")
